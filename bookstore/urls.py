f66e7a94edda1f70fd835cecfac5330004932a74
James Carignan <xpander@gmail.com>
2015-06-19 20:50:00 -0600

    Add requirements file and registration module, templates and URL

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^store/', include('store.urls'), name='store'),
+    url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
