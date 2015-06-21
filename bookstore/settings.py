ba2990bbb5696fb0581b405aa2fafae349919a38
James Carignan <xpander@gmail.com>
2015-06-20 19:11:05 -0600

    Add login/logout template, test session variables

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -106,3 +106,13 @@ STATIC_URL = '/static/'
 # Registration
 ACCOUNT_ACTIVATION_DAYS = 7
 REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = '/store/'
+
+# Email settings
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "xpander@gmail.com"
+EMAIL_HOST_PASSWORD = "uqsbxkcmmapvvkfo"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = "books@mysterybooks.com"
